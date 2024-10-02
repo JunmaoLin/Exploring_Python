5f8a9d60dcfb5d1e0e3e8edf05479991833a5ccb
JunmaoLin <junmaol02@gmail.com>
2024-10-02 04:09:38 -0400

    Create file8.py

---
+++ file8.py
@@ -0,0 +1,31 @@
+# Junmao Lin
+# string reverse
+
+def reverse1(s):
+    new_string = ""
+    for ch in s:
+        new_string = ch + new_string
+    return new_string
+
+def reverse2(s):
+    new_string = s[len(s)-1::-1] #starting at index len()-1
+    return new_string
+
+
+if __name__ == "__main__":
+    print("reverse1(\"Mississippi\") is:", reverse1("Mississippi"))
+    print()
+    print("reverse2(\"Mississippi\") is:", reverse2("Mississippi"))
+    print()
+    print("reverse1(\"Connetquot\") is:", reverse1("Connetquot"))
+    print()
+    print("reverse2(\"Connetquot\") is:", reverse2("Connetquot"))
+    print()
+    print("reverse1(\"Wyandanch\") is:", reverse1("Wyandanch"))
+    print()
+    print("reverse2(\"Wyandanch\") is:", reverse2("Wyandanch"))
+    print()
+    print("reverse1(\"Ronkonkoma\") is:", reverse1("Ronkonkoma"))
+    print()
+    print("reverse2(\"Ronkonkoma\") is:", reverse2("Ronkonkoma"))
+    print()
